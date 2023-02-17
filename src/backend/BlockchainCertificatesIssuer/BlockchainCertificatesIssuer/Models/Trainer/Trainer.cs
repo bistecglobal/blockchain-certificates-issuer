19fff82293c9414fd2d8034ed827f86bb9f570f9
pacmadhushan <chathuramadhushanpa@gmail.com>
2023-02-18 00:53:02 +0530

    resolve lastname speling mistake trainer and trainee object

--- src/backend/BlockchainCertificatesIssuer/BlockchainCertificatesIssuer/Models/Trainer/Trainer.cs
+++ src/backend/BlockchainCertificatesIssuer/BlockchainCertificatesIssuer/Models/Trainer/Trainer.cs
@@ -10,7 +10,7 @@ namespace BlockchainCertificatesIssuer.domain.Models.Trainer
     public class Trainer : Item
     {
         public string FirstName { get; set; }
-        public string LasttName { get; set; }
+        public string LastName { get; set; }
         public string EmailAddress { get; set; }
     }
 }
