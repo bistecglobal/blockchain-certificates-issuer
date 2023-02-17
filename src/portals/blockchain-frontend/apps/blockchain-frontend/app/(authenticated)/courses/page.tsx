c0ce3bc8831a650c289bcf1fd0d2defbe5f44847
Nalin Bandara <nalin@bistecglobal.com>
2023-02-17 12:58:53 +0530

    create ui part trainee and course

--- src/portals/blockchain-frontend/apps/blockchain-frontend/app/(authenticated)/courses/page.tsx
+++ src/portals/blockchain-frontend/apps/blockchain-frontend/app/(authenticated)/courses/page.tsx
@@ -1,20 +1,93 @@
 "use client";
 import styles from './cart-page-course.module.css';
-import { useEffect, useState } from "react";
 import { Avatar, Rate, Space, Table, Typography } from "antd";
-
-
+import {Form} from 'antd'
+// import Input from 'antd/es/input';
+import Button from 'antd/es/button';
+import {PlusOutlined} from '@ant-design/icons/lib/icons'
+import axios from 'axios';
+import React,{useState} from 'react';
+import { Descriptions } from 'antd';
+import {DatePicker} from 'antd'
+import { Input } from 'antd';
 /* eslint-disable-next-line */
 export interface CartPageCourseProps {}
 
 export function Courses(props: CartPageCourseProps) {
+  const [firstval,setfirstnameval]= useState("");
+  const [lastval,setlastnameval]= useState("");
+  const [emailval,setemailval]= useState("");
+  const { TextArea } = Input;
   
+  const setFirstNameVal = (value:any) =>{
+    setfirstnameval(value);
+};
+  const setLastNameVal  = (value:any) =>{
+  setlastnameval(value);
+ };
+
+  const setEmailVal = (value:any) =>{
+      setemailval(value);
+  };
+  
+  const handleSave =(e:React.MouseEvent<HTMLButtonElement, MouseEvent>)=>{
+    e.preventDefault();
+  const data ={
+    FirstName: firstval,
+    LastName: lastval,
+    Email : emailval,
+   
+  };
+  console.log("abc",data)
+  const url ='';
+  axios.post(url,data).then((result)=>{
+     alert(result.data);
+
+  }).catch((error)=>{
+    alert(error);
+  });
+  
+}
 
   return (
     <div className={styles['container']}>
-      <h1>Welcome to CartPageCourse!</h1>
+      
       <Typography>Courses</Typography>
+      <Form >
+      <Form.Item 
+        
+      rules={[
+        {
+          required:true,
+          message:"Title is required"
+        }
+      ]}
+      name ={"Course"} 
      
+       >
+      <Input placeholder='C#' value={lastval} 
+      onChange={(e)=>{setFirstNameVal(e.target.value)}} />
+      </Form.Item>
+    
+    
+  
+  <p>Start date</p>
+     <Space>
+     <div ><DatePicker/></div>
+     </Space>
+
+     <p>End Date</p>
+     <Space>
+     <div ><DatePicker/></div>
+     </Space>
+     <p>Description</p>
+     <TextArea rows={4} title="Description" name='Description' />
+    <br />
+    <br />
+     <Form.Item>
+      <Button htmlType="submit" icon={<PlusOutlined/>} type="primary"   onClick={(e)=>{ handleSave(e) }}> Add Courses</Button>
+     </Form.Item>
+     </Form>
     </div>
   );
 }
