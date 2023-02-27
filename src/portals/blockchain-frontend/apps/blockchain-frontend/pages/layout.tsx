e96aed210e649470854bc0b3fce865dbbf3b290d
Nalin Bandara <nalin@bistecglobal.com>
2023-02-27 19:21:01 +0530

    change the side menu

--- src/portals/blockchain-frontend/apps/blockchain-frontend/pages/layout.tsx
+++ src/portals/blockchain-frontend/apps/blockchain-frontend/pages/layout.tsx
@@ -1,15 +1,63 @@
 import React from 'react'
-import {SideMenu} from '../components/sideMenu';
+import styles from './layout.module.css';
+import {
+  AppstoreOutlined,
+  ShopOutlined,
+  UserOutlined,
+  SafetyCertificateOutlined,
+} from "@ant-design/icons";
+import { Menu } from "antd";
+import { useRouter } from "next/navigation";
+
 
 
 export default function RootLayout({ children }) {
 
-    
+    const navigate = useRouter();
     return (
         <div>
            
            <main>{children}</main> 
-            <SideMenu/>
+           <div className={styles["container"]}>
+      <div className={styles["SideMenu"]}>
+      <Menu
+        className={styles["SideMenuVertical"]}
+        mode="vertical"
+        onClick={(item) => {
+          //item.key
+          navigate.push(item.key);
+        }}
+        // selectedKeys={[selectedKeys]}
+        items={[
+          {
+            label: "Home",
+            icon: <AppstoreOutlined />,
+            key: "/home",
+          },
+          {
+            label: "Courses",
+            key: "/courses",
+            icon: <ShopOutlined />,
+          },
+          {
+            label: "Trainers",
+            key: "/trainers",
+            icon: <UserOutlined/>,
+          },
+          {
+            label: "Trainees",
+            key: "/trainees",
+            icon: <UserOutlined />,
+          },
+          {
+            label: "Certificates",
+            key: "/certificates",
+            icon: <SafetyCertificateOutlined />
+          },
+        ]}
+      ></Menu>
+    </div>
+    </div>
         
            
         </div>
