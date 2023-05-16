c226cebe5c76ed75f65b678f83fa4cd3b75a0441
Chamal Kaviranga | BISTEC Global <chamal@bistecglobal.com>
2023-05-16 13:03:30 +0530

    created trainers

--- src/portals/blockchain-frontend/apps/blockchain-frontend/components/trainers-container/TrainersContainer.tsx
+++ src/portals/blockchain-frontend/apps/blockchain-frontend/components/trainers-container/TrainersContainer.tsx
@@ -47,7 +47,7 @@ export default function TrainersContainer() {
             },
         },
     ];
-    useFetchTrainersEffect(fetchTrainers)
+
     const handlePaginationChange = (pageNumber: number, pageSize: number | undefined) => {
         fetchTrainers(pageNumber, pageSize ?? DefaultPagination.pageSize);
     };
