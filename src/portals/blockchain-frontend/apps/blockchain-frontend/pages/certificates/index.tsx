0cb689f37f1e598f88279265d306e598488eddc4
Chandima Ranaweera <chandima@bistecglobal.com>
2023-06-09 10:51:55 +0530

    Fix api urls

--- src/portals/blockchain-frontend/apps/blockchain-frontend/pages/certificates/index.tsx
+++ src/portals/blockchain-frontend/apps/blockchain-frontend/pages/certificates/index.tsx
@@ -1,195 +1,238 @@
-
 import styles from './cart-page-certificates.module.css';
-import {Select,Form,Button} from 'antd'
-import {PlusOutlined} from '@ant-design/icons/lib/icons'
-import {DatePicker} from 'antd'
+import { Select, Form, Button, notification, DatePicker } from 'antd';
+import { PlusOutlined } from '@ant-design/icons/lib/icons';
 import axios from 'axios';
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { DownloadOutlined } from '@ant-design/icons';
-import {exportComponentAsPNG } from "react-component-export-image";
 import { EthProvider } from 'apps/blockchain-frontend/contexts/EthContext';
 import BlockchainVerifier from 'apps/blockchain-frontend/components/blockchain-verifier/BlockchainVerifier';
-import FormItem from 'antd/es/form/FormItem';
-// import { useRouter } from 'next/router';
-import { useRouter } from "next/navigation";
 
-// export interface CartPageCertificatesProps {}
+import { useRouter } from 'next/navigation';
+import Image from 'next/image';
+
 
 export function CartPageCertificates() {
+  const [api, contextHolder] = notification.useNotification();
 
-  
-
-  const [courseval,setselectcourse]= useState("");
-  const [traineeval,setselecttrainee]= useState("");
-  const [trainerval,setselecttranier]= useState("");
+  const [courseval, setselectcourse] = useState('');
+  const [traineeval, setselecttrainee] = useState('');
+  const [trainerval, setselecttranier] = useState('');
   const [date, setDate] = React.useState(false);
+  const [courses, setCourses] = useState([]);
+  const [tainees, setDataTrainee] = useState([]);
+  const [trainers, setDataTrainer] = useState([]);
 
-  const navigate =useRouter();
+  const navigate = useRouter();
 
   const certificateWrapper = React.createRef<HTMLDivElement>();
 
-  
-  const setSelectCourse = (value) =>{
+  const setSelectCourse = (value) => {
     setselectcourse(value);
-};
-  const setSelectTrainee  = (value) =>{
+  };
+  const setSelectTrainee = (value) => {
     setselecttrainee(value);
- };
+  };
 
-  const setSelectTrainer = (value) =>{
+  const setSelectTrainer = (value) => {
     setselecttranier(value);
   };
-  function setIssueDate (date,dateString){
+  function setIssueDate(date, dateString) {
     setDate(dateString);
   }
-  const [data,setData2] =useState([]);
-  useEffect(()=>{
+
+  useEffect(() => {
     getData();
-  },[]);
-  
-  const getData =() =>{
-   
-    axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}api/CourseGetAPI?pageSize=10&pageNumber=1`)
-    .then((result)=>{
-      setData2(result.data);
-     console.log(result);
-       
-      })
-    .catch((error)=>{
-      console.log(error)
-    })  
-  }
-  const [datatrinee,setDataTrainee] =useState([]);
-  useEffect(()=>{
     getDataTrainee();
-  },[]);
-  
-  const getDataTrainee =() =>{
-   
-    axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}api/TraineeGetAPI?pageSize=10&PageNumber=1`)
-    .then((result)=>{
-      setDataTrainee(result.data);
-     console.log(result);
-       
-      })
-    .catch((error)=>{
-      console.log(error)
-    })  
-  }
-  const [datatriner,setDataTrainer] =useState([]);
-  useEffect(()=>{
     getDataTrainer();
-  },[]);
-  
-  const getDataTrainer =() =>{
-   
-    axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}api/TrainerGetAPI?pageSize=5&PageNumber=1`)
-    .then((result)=>{
-      setDataTrainer(result.data);
-     console.log(result);
-       
+  }, []);
+
+  const getData = () => {
+    axios
+      .get(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/api/courses?pageSize=10&pageNumber=1`
+      )
+      .then((result) => {
+        setCourses(result.data);
+        console.log(result);
       })
-    .catch((error)=>{
-      console.log(error)
-    })  
-  }
-
-
-  const handleSave =(e:React.MouseEvent<HTMLButtonElement, MouseEvent>)=>{
-    e.preventDefault();
-  const data ={
-    Course: courseval,
-    Trainee: traineeval,
-    Trainer : trainerval,
-    CertificateIssueDateDate: date
-   
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
-  const url =`${process.env.NEXT_PUBLIC_BASE_URL}api/Certificate`;
-  axios.post(url,data).then((result)=>{
-     alert(result.data);
+  const getDataTrainee = () => {
+    axios
+      .get(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/api/trainees?pageSize=10&PageNumber=1`
+      )
+      .then((result) => {
+        setDataTrainee(result.data);
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-  }).catch((error)=>{
-    alert(error);
-  });
-  }
- 
+  const getDataTrainer = () => {
+    axios
+      .get(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/api/trainers?pageSize=5&PageNumber=1`
+      )
+      .then((result) => {
+        setDataTrainer(result.data);
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleSave = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    const data = {
+      Course: courseval,
+      Trainee: traineeval,
+      Trainer: trainerval,
+      CertificateIssueDateDate: date,
+    };
+    const key = 'updatable';
+    const url = `${process.env.NEXT_PUBLIC_BASE_URL}/api/certificates`;
+    axios
+      .post(url, data)
+      .then((result) => {
+        console.log(result.data);
+        api.open({
+          key,
+          message: 'Issue Cerificate.',
+          description: 'Certificate created successfully',
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   return (
     <div>
-      
-    <EthProvider>
-      <BlockchainVerifier setValue={undefined}/>
+      {contextHolder}
+      <EthProvider>
+        <BlockchainVerifier setValue={undefined} />
       </EthProvider>
-    <div className={styles['container']}>
-      <div className={styles['content']}>
-      <Form >
-      <p>Select Course</p>
-      <Select   placeholder='Select Course' style={{width:150}} value={courseval} onChange={(value) => {setSelectCourse(value) }}  >
-        {data.map((course,index)=>(
-           <Select.Option 
-         key={index} value={course.Title}>{course.Title}</Select.Option> 
-        ))}
-      </Select>
-        
-    
-      <p>Select Trainee</p>
-      <Select   placeholder='Select Trainee' style={{width:150}} value={traineeval} onChange={(value) => {setSelectTrainee(value) }}   >
-        {datatrinee.map((trainee,index)=>(
-           <Select.Option 
-         key ={index} value={trainee.FirstName} >{trainee.FirstName}  </Select.Option>
-        ))}
-      </Select>
-      <p>Select Trainer</p>
-      <Select   placeholder='Select Trainer' style={{width:150}} value={trainerval} onChange={(value) => {setSelectTrainer(value) }} >
-      {datatriner.map((trainer,index)=>(
-           <Select.Option 
-         key ={index} value={trainer.FirstName} >{trainer.FirstName}  </Select.Option>
-        ))}
-      </Select>
-      <p>Select Certificate Issue Date</p>
-     
-        <div><DatePicker onChange={setIssueDate}/></div>
-      
-      <p></p>
-      <Form.Item>
-      <Button htmlType="submit" icon={<PlusOutlined/>} type="primary"   onClick={(e)=>{ handleSave(e) }}>Issue Certificate</Button>
-     </Form.Item>
-     {/* <Button type="primary" icon={<DownloadOutlined />} onClick={pdfGenerate}>
+      <div className={styles['container']}>
+        <div className={styles['content']}>
+          <Form>
+            <p>Select Course</p>
+            <Select
+              placeholder="Select Course"
+              style={{ width: 150 }}
+              value={courseval}
+              onChange={(value) => {
+                setSelectCourse(value);
+              }}
+            >
+              {courses.map((course, index) => (
+                <Select.Option key={index} value={course.Title}>
+                  {course.Title}
+                </Select.Option>
+              ))}
+            </Select>
+
+            <p>Select Trainee</p>
+            <Select
+              placeholder="Select Trainee"
+              style={{ width: 150 }}
+              value={traineeval}
+              onChange={(value) => {
+                setSelectTrainee(value);
+              }}
+            >
+              {tainees.map((trainee, index) => (
+                <Select.Option key={index} value={trainee.FirstName}>
+                  {trainee.FirstName}{' '}
+                </Select.Option>
+              ))}
+            </Select>
+            <p>Select Trainer</p>
+            <Select
+              placeholder="Select Trainer"
+              style={{ width: 150 }}
+              value={trainerval}
+              onChange={(value) => {
+                setSelectTrainer(value);
+              }}
+            >
+              {trainers.map((trainer, index) => (
+                <Select.Option key={index} value={trainer.FirstName}>
+                  {trainer.FirstName}{' '}
+                </Select.Option>
+              ))}
+            </Select>
+            <p>Select Certificate Issue Date</p>
+
+            <div>
+              <DatePicker onChange={setIssueDate} />
+            </div>
+
+            <p></p>
+            <Form.Item>
+              <Button
+                htmlType="submit"
+                icon={<PlusOutlined />}
+                type="primary"
+                onClick={(e) => {
+                  handleSave(e);
+                }}
+              >
+                Issue Certificate
+              </Button>
+            </Form.Item>
+            {/* <Button type="primary" icon={<DownloadOutlined />} onClick={pdfGenerate}>
             Download
           </Button> */}
 
-      <div className={styles['Meta']}>
-      <div className={styles['certificateWrapper']} ref={certificateWrapper}>
-        <p className={styles['p1']}>{courseval}</p>
-        <p className={styles['p2']}>{traineeval}</p>
-        <p className={styles['p3']}>{date}</p>
-        <img src="https://i.imgur.com/km7ATll.png" width={500} height={500} alt="" />
+            <div className={styles['Meta']}>
+              <div
+                className={styles['certificateWrapper']}
+                ref={certificateWrapper}
+              >
+                <p className={styles['p1']}>{courseval}</p>
+                <p className={styles['p2']}>{traineeval}</p>
+                <p className={styles['p3']}>{date}</p>
+                <Image
+                  src="/issue-cert.png"
+                  width={500}
+                  height={500}
+                  alt=""
+                />
+              </div>
+            </div>
+            <br />
+            <br />
 
-        
-        </div>  
+            <Button
+              style={{
+                position: 'absolute',
+                width: 110,
+                left: '98%',
+                top: '120%',
+              }}
+              type="primary"
+              icon={<DownloadOutlined />}
+              onClick={async (e) => {
+                e.preventDefault();
+                const { exportComponentAsPNG } = await import(
+                  'react-component-export-image'
+                );
+                exportComponentAsPNG(certificateWrapper);
+              }}
+            >
+              Download
+            </Button>
+          </Form>
         </div>
-        <br/><br/>
-       
-        <Button style={{position:'absolute',
-                width:110,
-                left:"98%",
-                top:"120%"         
-        
-      }} type="primary" icon={<DownloadOutlined />} 
-        onClick={async (e)=>{
-          e.preventDefault();
-          const { exportComponentAsPNG } = await import('react-component-export-image')
-         exportComponentAsPNG(certificateWrapper)
-          }}>
-            Download
-          </Button>
-        
-     </Form>
+      </div>
     </div>
-    </div>
-    </div>
-   
   );
 }
 
