f63859d659973711c421f184b133b5071562a33b
Lakmina Egodawattha <lakmina@bistecglobal.com>
2023-10-28 00:38:27 +0530

    added cards

--- src/portals/blockchain-frontend/apps/blockchain-frontend/components/courses-container/CourseCardView.tsx
+++ src/portals/blockchain-frontend/apps/blockchain-frontend/components/courses-container/CourseCardView.tsx
@@ -1,4 +1,4 @@
-import { DeleteOutlined, ArrowsAltOutlined } from '@ant-design/icons';
+import { DeleteOutlined, EditOutlined } from '@ant-design/icons';
 import SpinnerContainer from '../shared/spinner/SpinnerContainer';
 import { useComponentState } from './state';
 import PaginationContainer from '../shared/pagination/PaginationContainer';
@@ -6,44 +6,89 @@ import DeleteConfirmationModal from '../shared/deleteConfirmation/DeleteConfirma
 import { CourseResponse } from '../../interfaces/viewModels';
 
 export default function CourseCardView() {
-    const { dataSource, loading, handlePaginationChange, openDeleteModal, isDeleteModalOpen, closeDeleteModal, confirmDelete, total } = useComponentState();
-    return (
-        <>
-            {dataSource.length > 0 ? (
-                <div>
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                        {dataSource.map((course: CourseResponse, index) => (
-                            <div key={index} className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                                <div className="flex">
-                                    <div className="ml-auto">
-                                    <a href={`/courses?view=edit&id=${course.Id}`}><ArrowsAltOutlined /></a>
-                                    </div>
-                                </div>
-                                <div>
-                                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{course.Title}</h5>
-                                </div>
+  const {
+    dataSource,
+    loading,
+    handlePaginationChange,
+    openDeleteModal,
+    isDeleteModalOpen,
+    closeDeleteModal,
+    confirmDelete,
+    total,
+  } = useComponentState();
 
-                                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400"></p>
-                                <div className="flex">
-                                    <div className="ml-auto">
-                                        <button onClick={() => {
-                                            openDeleteModal(course.Id, course.Type);
-                                        }}><DeleteOutlined /></button>
+  const formatDate = (date) =>
+    new Date(date).toLocaleDateString(undefined, {
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric',
+    });
 
-                                    </div>
-                                </div>
-                            </div>
-                        ))}
-                    </div>
-                    <div><PaginationContainer data={dataSource} handlePaginationChange={handlePaginationChange} total={total} /></div></div>)
-                : (<div className='fixed inset-0 ml-40 z-[-1] flex items-center justify-center'>
+  return (
+    <>
+      {dataSource.length > 0 ? (
+        <div>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 p-2">
+            {dataSource.map((course: CourseResponse, index) => (
+              <div
+                key={index}
+                className="w-auto bg-white rounded-b-lg border-t-4 border-green-400 px-4 py-5 flex flex-col justify-around shadow-md "
+              >
+                <p className="text-lg font-bold font-sans"> {course.Title}</p>
+                <div className="py-3">
+                  <p className="text-gray-400 text-sm">{course.Description}</p>
+                </div>
 
-                    {loading ? (<div>
-                        <SpinnerContainer />
-                    </div>) : (<div className="text-lg text-gray-600">No data available.</div>)}</div>)}
+                <div className="flex justify-between mt-2">
+                  <p className="text-black-400 text-sm font-bold">
+                    {`${formatDate(course.StartDate)} to ${formatDate(
+                      course.EndDate
+                    )}`}
+                  </p>
+                  <div className="text-sm flex gap-2">
+                    <button className="bg-blue-200 px-3 py-0.5 rounded-xl hover:bg-blue-400 transition-colors ease-in-out">
+                      <a href={`/courses?view=edit&id=${course.Id}`}>
+                        <EditOutlined className="mb-1" />
+                      </a>
+                    </button>
+                    <button
+                      className="bg-red-200 px-3 py-0.5 rounded-xl hover:bg-red-400 transition-colors ease-in-out"
+                      onClick={() => {
+                        openDeleteModal(course.Id, course.Type);
+                      }}
+                    >
+                      <DeleteOutlined className="mb-1" />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div>
+            <PaginationContainer
+              data={dataSource}
+              handlePaginationChange={handlePaginationChange}
+              total={total}
+            />
+          </div>
+        </div>
+      ) : (
+        <div className="fixed inset-0 ml-40 z-[-1] flex items-center justify-center">
+          {loading ? (
+            <div>
+              <SpinnerContainer />
+            </div>
+          ) : (
+            <div className="text-lg text-gray-600">No data available.</div>
+          )}
+        </div>
+      )}
 
-
-            <DeleteConfirmationModal isOpen={isDeleteModalOpen} onCancel={closeDeleteModal} onConfirm={confirmDelete} />
-        </>
-    );
+      <DeleteConfirmationModal
+        isOpen={isDeleteModalOpen}
+        onCancel={closeDeleteModal}
+        onConfirm={confirmDelete}
+      />
+    </>
+  );
 }
