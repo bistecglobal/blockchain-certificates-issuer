4931b512db48095e6269353a64bd8e86f2116af3
Jayath de Silva <gitdevjay@gmail.com>
2023-11-03 12:44:39 +0530

    update  Card UI

--- src/portals/blockchain-frontend/apps/blockchain-frontend/components/trainers-container/TrainerMain.tsx
+++ src/portals/blockchain-frontend/apps/blockchain-frontend/components/trainers-container/TrainerMain.tsx
@@ -1,35 +1,97 @@
-import { useState } from "react";
-import CourseCardView from "./TrainerCardView";
-import { useComponentState } from "./state";
-import { PlusOutlined ,ArrowLeftOutlined} from '@ant-design/icons';
-import CourseForm from "./TrainerForm";
+import React, { useState } from 'react';
+//import CourseCardView from './TrainerCardView';
+import { useComponentState } from './state';
+import { PlusOutlined, ArrowLeftOutlined } from '@ant-design/icons';
+import TrainerForm from './TrainerForm';
+import TrainerCardView from './TrainerCardView';
+import { useRouter } from 'next/router';
 
 export default function TrainerMain() {
-    const { dataSource } = useComponentState();
-    const [isCardView, setIsCardView] = useState(true);
-    const onClickHandler = () => switchToCardView();
-    const switchToCardView = () => {
-        setIsCardView(!isCardView);
-    };
-    const btnTitle = isCardView ? 'Add a Trainer' : 'Back';
-    const title = isCardView ? 'Trainers' : 'Add a Trainer';
-    return (
-        <>
-        <div className="mx-auto px-4 py-16 text-center"> 
-            <div className="text-center md:text-left mt-8">
-                <span className="ml-8 mt-4 text-2xl font-bold">{title}</span>
-            </div>
-            <div className="ml-8 mt-4 text-center md:text-left">
-                <button className="bg-blue-500 text-white py-2 px-4 rounded flex items-center space-x-2 hover:bg-blue-600" onClick={onClickHandler} >
-                    {isCardView ?(
-                   <PlusOutlined  />) : (<ArrowLeftOutlined />)}
-                    <span>{btnTitle}</span>
-                </button>
-            </div>
-            <div className="mt-8 ml-8">
-            {isCardView ?  <CourseCardView  trainersData={dataSource} /> : <CourseForm />}
-               
-            </div>
+  const { view, id } = useComponentState();
+  const router = useRouter();
+  const [isCardView, setIsCardView] = useState(view === 'card');
+  let content = null;
+  let btnTitle = '';
+  let title = '';
+  let icon = null;
+
+  /*const { dataSource } = useComponentState();
+  const [isCardView, setIsCardView] = useState(true);
+  const onClickHandler = () => switchToCardView();
+  const switchToCardView = () => {setIsCardView(!isCardView);};
+  const btnTitle = isCardView ? 'Add a Trainer' : 'Back';
+  const title = isCardView ? 'Trainers' : 'Add a Trainer';*/
+
+  if (view === 'form') {
+    content = <TrainerForm />;
+    btnTitle = 'Back';
+    title = 'Add a Trainer';
+    icon = <ArrowLeftOutlined />;
+  } else if (id) {
+    content = <TrainerForm />;
+    btnTitle = 'Back';
+    title = 'Edit Trainer';
+    icon = <ArrowLeftOutlined />;
+  } else {
+    content = <TrainerCardView />;
+    btnTitle = 'Add a Trainer';
+    title = 'Trainees';
+    icon = <PlusOutlined />;
+  }
+
+  const toggleView = () => {
+    if (isCardView) {
+      router.push(`?view=card`);
+    } else {
+      router.push(`?view=form`);
+    }
+    setIsCardView(!isCardView);
+  };
+
+  return (
+    <>
+      <div className="mx-auto px-4 py-16 text-center">
+        <div className="text-center md:text-left mt-8">
+          <span className="ml-8 mt-4 text-2xl font-bold">{title}</span>
         </div>
-        </>
-    );}
+        <div className="ml-8 mt-4 text-center md:text-left">
+          <button
+            className="bg-blue-500 text-white py-2 px-4 rounded flex items-center space-x-2 hover:bg-blue-600"
+            onClick={toggleView}
+          >
+            {icon}
+            <span>{btnTitle}</span>
+          </button>
+        </div>
+        <div className="mt-8 ml-8">{content}</div>
+      </div>
+      {/*
+    <>
+      <div className="mx-auto px-4 py-16 text-center">
+        <div className="text-center md:text-left mt-8">
+          <span className="ml-8 mt-4 text-2xl font-bold">{title}</span>
+        </div>
+        <div className="ml-8 mt-4 text-center md:text-left">
+          <button
+            className="bg-blue-500 text-white py-2 px-4 rounded flex items-center space-x-2 hover:bg-blue-600"
+            onClick={onClickHandler}
+          >
+            {isCardView ? <PlusOutlined /> : <ArrowLeftOutlined />}
+            <span>{btnTitle}</span>
+          </button>
+        </div>
+        
+        <div className="mt-8 ml-8">
+          {isCardView ? (
+            <CourseCardView trainersData={dataSource} />
+          ) : (
+            <CourseForm />
+          )}
+        </div>
+        
+      </div>
+    </>
+  */}
+    </>
+  );
+}
--- src/portals/blockchain-frontend/apps/blockchain-frontend/components/trainers-container/state.tsx
+++ src/portals/blockchain-frontend/apps/blockchain-frontend/components/trainers-container/state.tsx
@@ -1,127 +1,157 @@
-import { createTrainer, deleteById, getTrainers } from "apps/blockchain-frontend/api/fetchData";
-import { PaginationResponse, TrainerRequest, TrainerResponse } from "apps/blockchain-frontend/interfaces/viewModels";
-import { useEffect, useState } from "react";
+import {
+  createTrainer,
+  deleteById,
+  getTrainers,
+} from 'apps/blockchain-frontend/api/fetchData';
+import {
+  PaginationResponse,
+  TrainerRequest,
+  TrainerResponse,
+} from 'apps/blockchain-frontend/interfaces/viewModels';
+import { useEffect, useState } from 'react';
 import { useFormik } from 'formik';
-import { DefaultPagination } from "apps/blockchain-frontend/interfaces/enums";
-import { Modal, message } from "antd";
-import {ExclamationCircleOutlined } from '@ant-design/icons/lib/icons';
+import { DefaultPagination } from 'apps/blockchain-frontend/interfaces/enums';
+import { Modal, message } from 'antd';
+import { ExclamationCircleOutlined } from '@ant-design/icons/lib/icons';
 import { useRouter } from 'next/router';
 
 export function useComponentState() {
-    const [dataSource, setDataSource] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [isCancelModalOpen, setIsCancelModalOpen] = useState(false);
-    const [isCancelDisable, setIsCancelDisable] = useState(true);
-    const router = useRouter();
-    const { query } = router;
-    const id = query.id || null
-    const [total, setTotal] = useState(0);
+  const [dataSource, setDataSource] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [isCancelModalOpen, setIsCancelModalOpen] = useState(false);
+  const [isCancelDisable, setIsCancelDisable] = useState(true);
+  const router = useRouter();
+  const { query } = router;
+  const id = query.id || null;
+  const defaultView = 'card';
+  const view = query.view || defaultView;
+  const [total, setTotal] = useState(0);
 
-    const createNewTrainer = async (values) => {
-      const trainer: TrainerRequest = {
-          FirstName : values.firstName,
-          LastName : values.lastName,
-          EmailAddress : values.emailAddress
-      };
-      const trainerRes = await createTrainer(trainer);
-      if(trainerRes){
-        message.success(`Trainer created successfully`);
-        fetchTrainers(DefaultPagination.pageNumber, DefaultPagination.pageSize);
-        clearForm();
-      }else{
-        message.success(`Failed to create the course`);
-      }
+  const createNewTrainer = async (values) => {
+    const trainer: TrainerRequest = {
+      FirstName: values.firstName,
+      LastName: values.lastName,
+      EmailAddress: values.emailAddress,
     };
-  
-    const validate = (values) => {
-      const errors: {
-        firstName?: string;
-        lastName?: string;
-        emailAddress?: string;
-      } = {};
-  
-      if (!values.firstName) {
-        errors.firstName = 'First Name is required';
-      } 
-  
-      if (!values.lastName) {
-        errors.lastName = 'Last Name is required';
-      }
-  
-      if (!values.emailAddress) {
-        errors.emailAddress = 'Email is required';
-      }
-  
-      setIsCancelDisable(false);
-      return errors;
-    };
-  
-    const formik = useFormik({
-      initialValues: {
-        firstName: '',
-        lastName: '',
-        emailAddress: '',
-      },
-      validate,
-      onSubmit: createNewTrainer,
-    });
-
-    const handlePaginationChange = (pageNumber: number, pageSize: number | undefined) => {
-      fetchTrainers(pageNumber, pageSize ?? DefaultPagination.pageSize);
-    };
-  
-    const fetchTrainers = async (pageNumber: number, pageSize: number) => {
-      setLoading(true);
-      let response: PaginationResponse = await getTrainers(pageNumber, pageSize);
-      let trainerRes = response.Items;
-      setTotal(response.Total);
-      if (Array.isArray(trainerRes)) {
-        trainerRes = trainerRes.flat();
-      }
-      const formattedData = trainerRes.map((item) => {
-        return { ...item,key : item.Id};
-      });
-      setLoading(false);
-      setDataSource(formattedData);
- 
-    };
-    useEffect(() => {
+    const trainerRes = await createTrainer(trainer);
+    if (trainerRes) {
+      message.success(`Trainer created successfully`);
       fetchTrainers(DefaultPagination.pageNumber, DefaultPagination.pageSize);
-    }, []);  
-    
-    const handleDelete =( itemName, id) => {
-      Modal.confirm({
-        title: `Are you sure you want to delete this ${itemName}?`,
-        icon: <ExclamationCircleOutlined />,
-        onOk() {
-        deleteById(id,itemName).then((success) => {
-            if (success) {
-              message.success(`${itemName} deleted successfully`);
-              fetchTrainers(DefaultPagination.pageNumber, DefaultPagination.pageSize);
-            } else {
-              message.error(`Failed to delete ${itemName}`);
-            }
-          });
-        },
-        onCancel() { },
-      });
-    }  
-    const confirmCancel = () => {
-      if (!id) {
-        clearForm();
-      } 
-  
-      setIsCancelModalOpen(false);
-      setIsCancelDisable(true);
-    };
-    const handleCancel = () => {
-      setIsCancelModalOpen(true);
-    };
-    const closeModalOpen = () => {
-      setIsCancelModalOpen(false);
-    };
-    const clearForm = () => {
-      formik.resetForm();
-    };
-    return { formik,isCancelModalOpen,isCancelDisable,handleCancel,closeModalOpen,confirmCancel, handleDelete, dataSource, fetchTrainers, loading,id, total, handlePaginationChange };
-  }
+      clearForm();
+    } else {
+      message.success(`Failed to create the course`);
+    }
+  };
+
+  const validate = (values) => {
+    const errors: {
+      firstName?: string;
+      lastName?: string;
+      emailAddress?: string;
+    } = {};
+
+    if (!values.firstName) {
+      errors.firstName = 'First Name is required';
+    }
+
+    if (!values.lastName) {
+      errors.lastName = 'Last Name is required';
+    }
+
+    if (!values.emailAddress) {
+      errors.emailAddress = 'Email is required';
+    }
+
+    setIsCancelDisable(false);
+    return errors;
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      firstName: '',
+      lastName: '',
+      emailAddress: '',
+    },
+    validate,
+    onSubmit: createNewTrainer,
+  });
+
+  const handlePaginationChange = (
+    pageNumber: number,
+    pageSize: number | undefined
+  ) => {
+    fetchTrainers(pageNumber, pageSize ?? DefaultPagination.pageSize);
+  };
+
+  const fetchTrainers = async (pageNumber: number, pageSize: number) => {
+    setLoading(true);
+    let response: PaginationResponse = await getTrainers(pageNumber, pageSize);
+    let trainerRes = response.Items;
+    setTotal(response.Total);
+    if (Array.isArray(trainerRes)) {
+      trainerRes = trainerRes.flat();
+    }
+    const formattedData = trainerRes.map((item) => {
+      return { ...item, key: item.Id };
+    });
+    setLoading(false);
+    setDataSource(formattedData);
+  };
+  useEffect(() => {
+    fetchTrainers(DefaultPagination.pageNumber, DefaultPagination.pageSize);
+  }, []);
+
+  const handleDelete = (itemName, id) => {
+    Modal.confirm({
+      title: `Are you sure you want to delete this ${itemName}?`,
+      icon: <ExclamationCircleOutlined />,
+      onOk() {
+        deleteById(id, itemName).then((success) => {
+          if (success) {
+            message.success(`${itemName} deleted successfully`);
+            fetchTrainers(
+              DefaultPagination.pageNumber,
+              DefaultPagination.pageSize
+            );
+          } else {
+            message.error(`Failed to delete ${itemName}`);
+          }
+        });
+      },
+      onCancel() {},
+    });
+  };
+  const confirmCancel = () => {
+    if (!id) {
+      clearForm();
+    }
+
+    setIsCancelModalOpen(false);
+    setIsCancelDisable(true);
+  };
+  const handleCancel = () => {
+    setIsCancelModalOpen(true);
+  };
+  const closeModalOpen = () => {
+    setIsCancelModalOpen(false);
+  };
+  const clearForm = () => {
+    formik.resetForm();
+  };
+  return {
+    formik,
+    isCancelModalOpen,
+    isCancelDisable,
+    handleCancel,
+    closeModalOpen,
+    confirmCancel,
+    handleDelete,
+    dataSource,
+    fetchTrainers,
+    loading,
+    id,
+    view,
+    total,
+    handlePaginationChange,
+  };
+}
